3ba58915349c194808fe125e24936c0bac1ed0df
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-26 12:09:45 -0500

    if-else y anidación de if

--- expresiones.sh
+++ expresiones.sh
@@ -1,35 +1,15 @@
-echo -n "Ingresa numero 1: "
-read num1
-echo -n "Ingresa numero 2: "
-read num2
-if [ "$num1" -eq "$num2" ];
-then
-echo "$num1 es igual a $num2"
-fi
+echo -n "Ingresa nota: "
+read nota
 
-if [ "$num1" -ge "$num2" ];
+if [ "$nota" -ge 6 ];
 then
-echo "$num1 es mayor o igual a $num2"
-fi
-
-if [ "$num1" -le "$num2" ];
-then
-echo "$num1 es menor o igual a $num2"
-fi
-
-if [ "$num1" -ne "$num2" ];
-then
-echo "$num1 es diferente a $num2"
-fi
-
-if [ "$num1" -gt "$num2" ];
-then
-echo "$num1 es mayor a $num2"
-fi
-
-if [ "$num1" -lt "$num2" ];
-then
-echo "$num1 es menor a $num2"
+echo "Aprobaste"
+	if [ "$nota" -ge 9 ];
+	then
+	echo "Sobresaliente"
+	fi
+else
+echo "Reprobado"
 fi
 
 echo fin
