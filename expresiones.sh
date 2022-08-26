e673f3cfe83fa5367a3a8fccde7052043905fcfd
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-26 11:53:28 -0500

    Comparaciones numericas

--- expresiones.sh
+++ expresiones.sh
@@ -1,7 +1,35 @@
-echo -n "Ingresa pass:"
-read pass
-if [ "$pass" = "xyz" ];
+echo -n "Ingresa numero 1: "
+read num1
+echo -n "Ingresa numero 2: "
+read num2
+if [ "$num1" -eq "$num2" ];
 then
-echo Bienvenido
+echo "$num1 es igual a $num2"
 fi
+
+if [ "$num1" -ge "$num2" ];
+then
+echo "$num1 es mayor o igual a $num2"
+fi
+
+if [ "$num1" -le "$num2" ];
+then
+echo "$num1 es menor o igual a $num2"
+fi
+
+if [ "$num1" -ne "$num2" ];
+then
+echo "$num1 es diferente a $num2"
+fi
+
+if [ "$num1" -gt "$num2" ];
+then
+echo "$num1 es mayor a $num2"
+fi
+
+if [ "$num1" -lt "$num2" ];
+then
+echo "$num1 es menor a $num2"
+fi
+
 echo fin
