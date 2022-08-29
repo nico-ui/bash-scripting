5fa2c6e03b2d34b0cbd5fdabf89363d411d76c2b
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-28 21:32:13 -0500

    calculadora con funciones

--- calculadora.sh
+++ calculadora.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
+function suma() {
+	let resultado=$1+$2
+	echo $resultado
+}
+function resta() {
+	let resultado=$1-$2
+	echo $resultado
+}
+function multiplicacion() {
+	let resultado=$1*$2
+	echo $resultado
+}
+function division() {
+	let resultado=$1/$2
+	echo $resultado
+}
 echo "MENU:"
 echo "1. suma"
 echo "2. resta"
 echo "3. multipicacion"
 echo "4. division"
-echo "Elige una opcion: "; read opcion
-echo opcion: $opcion
-
-echo "Ingresa el primer numero:"; read x
-echo "Ingresa el segundo numero:"
-read y
-if [ "$opcion" -eq 1 ];
-then
-let suma=x+y
-echo suma: $suma
-fi
-if [ "$opcion" -eq 2 ];
-then
-let resta=x-y
-echo resta: $resta
-fi
-if [ "$opcion" -eq 3 ];
-then
-let multiplicacion=x*y
-echo multiplicacion: $multiplicacion
-fi
-if [ "$opcion" -eq 4 ];
-then
-let division=x/y
-echo division: $division
+echo -n "Elige una opcion: "; read opcion
+echo -n "Ingresa el primer numero: "; read x
+echo -n "Ingresa el segundo numero: "; read y
+if [ "$opcion" -eq 1 ]; then
+	suma $x $y
+elif [ "$opcion" -eq 2 ]; then
+	resta $x $y
+elif [ "$opcion" -eq 3 ]; then
+	multiplicacion $x $y
+elif [ "$opcion" -eq 4 ]; then
+	division $x $y
+else
+	echo "Opcion invalida"
 fi
