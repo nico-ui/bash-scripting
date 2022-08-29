547a7169e90b84a34c95109707d31a0c229cd5f5
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-28 22:05:32 -0500

    Ejercicio 7. Pago de cuota dependiendo de edad

--- expresiones.sh
+++ expresiones.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-echo -n "Ingresa contrasena: "; read -s x
-contra="arbol"
-if [ "$x" = "$contra" ]; then
-	echo "Bienvenido"
-else
-	echo "Acceso denegado"
+#si es mayor a 18 para 10
+#si esta entre 5 y 18 paga 5
+#si es menor a 4 no paga
+echo -n "Ingresa tu edad: "; read edad
+if [ "$edad" -gt 18 ]; then
+	echo "Cuota de $ 10"
+elif [ "$edad" -ge 5 ]; then
+	echo "Cuota de $ 5"
+elif [ "$edad" -lt 0 ]; then
+	echo "Edad invalida"
+elif [ "$edad" -le 4 ]; then
+	echo "No paga cuota"
 fi
