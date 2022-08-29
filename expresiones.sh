4e2e0967f41c87354b79073b17fd40c14c902da1
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-28 21:41:02 -0500

    Ejercicio 5. Par o impar de un numero con operacion en if

--- expresiones.sh
+++ expresiones.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "Par o impar"
 echo -n "Ingresa un numero: "; read x
-let residuo=$x%2
-if [ "$residuo" -eq 0 ]; then
+if [ "$(($x%2))" -eq 0 ]; then
 	echo "Numero par"
 else
 	echo "Numero impar"
