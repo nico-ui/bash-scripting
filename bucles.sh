d7f9592dfa7ded70bf4a801a6e887f6f9b0e9892
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-30 12:22:13 -0500

    Ejercicio 6. Array con bucle

--- bucles.sh
+++ bucles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Recorrido de for contenido directorio"
-for x in *
+echo "Recorrido de for array con bucle"
+array=(perro gato lapiz)
+for x in ${array[*]}
 do
-	ls -l $x
+	echo $x
 	sleep 1
 done
