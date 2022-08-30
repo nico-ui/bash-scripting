7ca0deddc170613537d4d7e7cd8773e157fc7b21
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-30 14:03:58 -0500

    Ejercicio. Paradoja del cumpleanos

--- bucles.sh
+++ bucles.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-echo "for tipo C"
-echo -n "Ingresa el num de elementos: "; read tam
-for (( i=0; i<$tam; i=i+1 )); do
-	echo $i
+echo "Paradoja"
+ruta="C:/Users/XMX1283/Documents/Cursos/bash/output.txt"
+if [ -f $ruta ]; then 
+	echo $ruta
+	echo "file existe"
+	sleep 1
+else
+	echo $ruta
+	echo "file no existe"
+	touch $ruta
+	sleep 1
+fi
+for x in {1..57}; do
+	echo $(($RANDOM%365)) >> $ruta	
 done
+sort $ruta
+rm $ruta
