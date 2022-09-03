0c215f504b981ca2ace5ac887a187823f894a55d
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-09-02 20:59:27 -0500

    Merge conflicto resuelto

--- expresiones.sh
+++ expresiones.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-echo -n "Ingresa nota: "
-read nota
-if [ "$nota" -ge 9 ]; then
-	echo "A"
-elif [ "$nota" -ge 7 ]; then
-	echo "B"
-else
-	echo "C"
-=======
 #si es mayor a 18 para 10
 #si esta entre 5 y 18 paga 5
 #si es menor a 4 no paga
@@ -21,5 +11,4 @@ elif [ "$edad" -lt 0 ]; then
 	echo "Edad invalida"
 elif [ "$edad" -le 4 ]; then
 	echo "No paga cuota"
->>>>>>> Ejercicios
 fi
