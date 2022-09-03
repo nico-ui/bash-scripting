de916dc3ec8bfa384cd44dccece75d5cfeb08546
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-09-02 20:17:41 -0500

    Ejercicio. While. Inicio sesión

--- bucles.sh
+++ bucles.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
-echo "Estructura while"
-echo -n "Ingresa tam: "; read tam
-let i=0
-while [ $i -le $tam ]; do
-	echo $i
-	let i=$i+1
+#/bin/bash
+clear
+echo -n "Introduce contrasena: "
+read pass
+let contador=1
+while [ "$pass" != "ABC" -a $contador -lt 5 ]; do	
+	echo "Contrasena equivocada, vuelve a intentarlo"
+	echo -n "Introduce contrasena: "
+	read pass
+	let contador=$contador+1
 done
+if [ $pass="ABC" ] ; then
+	echo "Inicio sesion correcto"
+fi
