16d04f41e212fed989a8a9a38cffa9a5aa0b3248
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-09-02 20:51:14 -0500

    Ejercicio. Until

--- bucles.sh
+++ bucles.sh
@@ -1,14 +1,8 @@
 #/bin/bash
-clear
-echo -n "Introduce contrasena: "
-read pass
-let contador=1
-while [ "$pass" != "ABC" -a $contador -lt 5 ]; do	
-	echo "Contrasena equivocada, vuelve a intentarlo"
-	echo -n "Introduce contrasena: "
-	read pass
-	let contador=$contador+1
+echo -n "Introduce un numero: "
+read num
+until [ "$num" -le 0 ]; 
+do	
+	echo "$num"
+	num=$(($num - 1))
 done
-if [ $pass="ABC" ] ; then
-	echo "Inicio sesion correcto"
-fi
