7a0ebfda72ce9348366c0988fe3913b9fb677143
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-09-03 20:28:12 -0500

    Ejercicio Funciones. Llamado a funcion 1

--- funciones.sh
+++ funciones.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-function suma(){
-	echo "Resultado de suma: " $(($1 + $2 + $3 + $4))
+function saludo(){
+	echo "Hola"
 }
-if [ $# -eq 4 ]; then
-	valor=$(suma $1 $2 $3 $4)
-	echo $valor
-else
-	echo "Opcion incorrecta, Ingrese parametros como: myFunction a b c d"
-fi
+echo $(saludo)
