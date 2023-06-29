ffb353bb6f92ffe8a81d7be615e09b803c5f8af4
cristiannicolai.hernandez.rio <cristiannicolai.hernandez.rio@bbva.com>
2023-06-29 13:24:05 -0600

    Se plantea pauta 1

---
+++ pauta1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#Escribid un script en Bash que acepte una ruta a un fichero o un directorio e imprima por pantalla si es un fichero normal, un directorio u otro tipo de fichero. Finalmente, el script ejecutará el comando ls sobre esta ruta en formato largo.
