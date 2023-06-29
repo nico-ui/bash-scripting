c7535a106a1d0d4a70829d99dd54d955cf0efa7d
cristiannicolai.hernandez.rio <cristiannicolai.hernandez.rio@bbva.com>
2023-06-29 13:29:41 -0600

    Version inicial pauta2

--- pauta2.sh
+++ pauta2.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #Escribid un script que ejecute cierta acción en función de la extensión de un archivo que recibe como parámetro. Si se trata de un JPG, copiar dicho archivo en la carpeta ~/fotos. Si resulta ser de otro formato, avisar al usuario sin hacer nada.
+#1. validar existencia
+echo "parametro $1"
+variable="locate $1"
+echo $variable
+if [ -f "$1" ]; then
+        echo si
+else
+        echo no se encontro archivo
+fi
+#2. logica
+if [ "${1##*.}" = "jpg" ]; then
+        echo extension igual a jpg
+        pwd
+        cd
+        pwd
+else
+        echo No es opcion valida
+fi
