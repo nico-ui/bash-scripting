9f4149ead066916af394b628d8a423cff35ae65d
cristiannicolai.hernandez.rio <cristiannicolai.hernandez.rio@bbva.com>
2023-06-29 13:25:30 -0600

    Se plantea pauta 2

---
+++ pauta2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#Escribid un script que ejecute cierta acción en función de la extensión de un archivo que recibe como parámetro. Si se trata de un JPG, copiar dicho archivo en la carpeta ~/fotos. Si resulta ser de otro formato, avisar al usuario sin hacer nada.
