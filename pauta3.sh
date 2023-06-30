9d3991a048feada39d72ae6c1332d4cae39f67e5
cristiannicolai.hernandez.rio <cristiannicolai.hernandez.rio@bbva.com>
2023-06-30 17:00:37 -0600

    Solucion pauta3

--- pauta3.sh
+++ pauta3.sh
@@ -2,9 +2,14 @@
 echo $0
 echo $#
 echo $1 $2
+var1=$@
+#echo var1:  $var1
+IFS=' ' read -ra var1 <<< "$var1"
 if [ $# -gt 2 ]; then
-        for (( c=$(($#-2)); c<=$#; c++ ))
+#       for c in ${var1[@]}
+        for (( c=2; c<=$#-1; c++ ))
         do
-                echo $($c)
+                echo ${var1[$c]}
+#               echo $c
         done
 fi
